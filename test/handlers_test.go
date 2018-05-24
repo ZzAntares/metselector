d1bf1c5661f2fa82f8634814abec64289e90bc29
Julio César <zzantares@gmail.com>
2018-05-24 18:04:40 -0500

    [HU3] Agrega test de prueba para tomar como base
    Las pruebas no unitarias se organizarán dentro de "test/", mientras
    que las unitarias se agregan junto con el archivo que contiene las
    funciones que se van a probar.

---
+++ test/handlers_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ZzAntares/metselector/internal/controllers"
+	"github.com/ZzAntares/metselector/internal/models"
+)
+
+type mockDB struct{}
+
+func (mdb *mockDB) AllQuestions() []*models.Question {
+	return make([]*models.Question, 0)
+}
+
+// Take this as a sample test
+func TestHealthCheckHandler(t *testing.T) {
+	rresponse := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/healthcheck", nil)
+
+	app := &controllers.App{Database: &mockDB{}}
+	http.HandlerFunc(app.HealthCheckHandler).ServeHTTP(rresponse, req)
+
+	var expected string = "\"Still alive!\"\n"
+	var obtained string = rresponse.Body.String()
+
+	if expected != obtained {
+		t.Errorf(
+			"\n...expected = %#v\n...obtained = %#v",
+			expected,
+			obtained)
+	}
+}
